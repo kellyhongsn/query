1c2417623c1f56c9b2b5bed28c9b2566c51577a0
“kellyhongsn” <kellyhongsn@berkeley.edu>
2024-07-28 13:10:02 -0400

    cors

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const OpenAI = require('openai');
-
+const cors = require('cors');
+app.use(cors());
 const app = express();
 const port = process.env.PORT || 3000;
 
